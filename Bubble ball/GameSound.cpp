2ea943c7ab3cb75c57e58254407099c9d5b8fa18
Vipadaaa <64010808@kmitl.ac.th>
2021-11-23 15:46:13 +0700

    implement sound and music

--- Bubble ball/GameSound.cpp
+++ Bubble ball/GameSound.cpp
@@ -1,31 +1,44 @@
 #include "GameSound.h"
-GameSound::GameSound(float width, float height)
-{
-	/*
-	if (!music.openFromFile("mixkit.wav"))
-	{
-		//handle eror
-	}
-	music.setVolume(30);
-	music.play();
-	music.setLoop(true);
-	
-	if (!buffer.loadFromFile("sound1.wav"))
-	{
-		//handle eror
-	}
-	sound.setBuffer(buffer);
-	sound.setVolume(40);
-	sound.play();
-	*/
-}
 
 GameSound::~GameSound()
 {
 
 }
 
-void GameSound::playMusic()
+GameSound::GameSound(int no)
 {
 
+	if (!music.openFromFile("music1.ogg")) {
+		//
+	}
+
+
+	if (!buffer.loadFromFile("sound1.wav")) {
+	}
+	sound.setBuffer(buffer);
+
+	if (!buffer2.loadFromFile("sound2.wav")) {
+	}
+	sound2.setBuffer(buffer2);
+
+
+	if (!buffer3.loadFromFile("sound3.wav")) {
+	}
+	sound3.setBuffer(buffer3);
+
+	
+}
+
+void GameSound::play(int no)
+{
+	if (no == 1) {
+		sound.play();
+	}
+	if (no == 2) {
+		sound2.play();
+	}
+	if (no == 3) {
+		sound3.play();
+	}
+	//music.play();
 }
